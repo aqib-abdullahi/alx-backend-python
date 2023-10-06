7c961d23334c70b0ec873ebdbf2d1bf66cf74520
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-10-06 17:39:12 +0000

    Advanced task 101

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Advanced task101
+"""
+from typing import TypeVar, Mapping, Any, Union
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+        dct: Mapping,
+        key: Any,
+        default: Union[T, None] = None) -> Union[Any, T]:
+    """safely gets value
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
