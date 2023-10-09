e81350e663b8b68233b971995e01ce0db3b118fd
aqibabdullahi <abdullahiaqib@gmail.com>
2023-10-09 22:36:20 +0100

    concurrent coroutines task1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""async routing: wait_n
+"""
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """async routine: Spawns wait_n n times with max_delay
+    """
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    return sorted(delays)
