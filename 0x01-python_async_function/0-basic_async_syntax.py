c2279c68aa581832c45258bbd373735b8a08afb8
aqibabdullahi <abdullahiaqib@gmail.com>
2023-10-09 21:40:48 +0100

    async waitint on random ints task0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Async task: wait_random
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """argument with default value of 10
+    that waits for a random delay between 0 ans max_delay
+    seconds and eventually returns it.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
