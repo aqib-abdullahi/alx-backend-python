e144b700325a86e50ae2e970837b2f46d390b24b
aqibabdullahi <abdullahiaqib@gmail.com>
2023-10-11 19:11:36 +0100

    async_generator function documented

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,9 @@ import asyncio
 
 
 async def async_generator() -> Generator[float, None, None]:
+    """asynchronously waits for a second and yields any
+    random float between 0 and 10
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
