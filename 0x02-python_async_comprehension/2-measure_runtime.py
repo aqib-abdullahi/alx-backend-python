72dd88cf95879aa0592edba4db35126e54f886a0
aqibabdullahi <abdullahiaqib@gmail.com>
2023-10-12 09:49:21 +0100

    neasures runtime for executing async_comprehension 4 times

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""measure_runtime task2
+"""
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """executes async_comprehension four times using asyncio.gather
+    """
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    finish_time = time.time() - start_time
+    return finish_time
