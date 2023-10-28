8aa715aa81fc3cf3c5a9d0451050a05b4f9884b6
aqibabdullahi <abdullahiaqib@gmail.com>
2023-10-28 20:56:37 +0100

    tests_public_repos_url task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,11 +3,12 @@
 """
 from parameterized import parameterized
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from utils import access_nested_map, get_json, memoize
 from typing import Dict, Union, Tuple
 import client
 from client import GithubOrgClient
+GithubOrgClient = __import__('client').GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -24,10 +25,21 @@ class TestGithubOrgClient(unittest.TestCase):
         with patch('client.get_json',
                    return_value={'org_name': 'TestOrg'}) as mock_get_json:
             client_org = GithubOrgClient(org).org
-            # client_org = client.org(GithubOrgClient.ORG_URL.format(org=org))
             called_once_param = GithubOrgClient.ORG_URL.format(org=org)
             mock_get_json.assert_called_once_with(called_once_param)
             self.assertEqual(client_org, {'org_name': 'TestOrg'})
+
+    def test_public_repos_url(self):
+        """tests the GithubOrgClient._public_repos_url
+        method
+        """
+        API = 'https://api.github.com/users/abc/repos'
+        repos_url_dict = {'repos_url': API}
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock,
+                   return_value=repos_url_dict) as mock_org:
+            self.assertEqual(GithubOrgClient('abc')._public_repos_url,
+                             'https://api.github.com/users/abc/repos')
 
 
 if __name__ == "__main__":
