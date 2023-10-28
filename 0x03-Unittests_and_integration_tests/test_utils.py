1f48bd6d64bd2d2866efeb55b245a996baec897c
aqibabdullahi <abdullahiaqib@gmail.com>
2023-10-28 17:07:06 +0100

    tests get_json task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@ utils.py
 """
 from parameterized import parameterized
 import unittest
-from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json
 from typing import Dict, Union, Tuple
 
 
@@ -42,6 +43,28 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Tests that get_json returns the expected result
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self,
+                      test_url: str,
+                      test_payload: Dict,
+                      ) -> None:
+        """mocks the return value of the requests.get
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        with patch('utils.requests.get',
+                   return_value=mock_response) as mock_get:
+            result = get_json(test_url)
+            self.assertEqual(result, test_payload)
+            mock_get.assert_called_once_with(test_url)
 
 
 if __name__ == "__main__":
