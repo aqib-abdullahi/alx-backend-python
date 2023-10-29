9163ea6cd2001465e59a10afd5a4650814e5b12e
aqibabdullahi <abdullahiaqib@gmail.com>
2023-10-29 21:23:22 +0100

    task8 and adv task

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -82,7 +82,7 @@ class TestGithubOrgClient(unittest.TestCase):
     {
         'org_payload': TEST_PAYLOAD[0][0],
         'repos_payload': TEST_PAYLOAD[0][1],
-        'expected_reops': TEST_PAYLOAD[0][2],
+        'expected_repos': TEST_PAYLOAD[0][2],
         'apache2_repos': TEST_PAYLOAD[0][3],
     }
 ])
@@ -108,6 +108,22 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
 
         cls.get_patcher = patch('requests.get', side_effect=get_payload)
         cls.get_patcher.start()
+
+    def test_public_repos(self) -> None:
+        """test the public_repos method
+        """
+        self.assertEqual(
+            GithubOrgClient('google').public_repos(),
+            self.expected_repos,
+        )
+
+    def test_public_repos_With_license(self):
+        """tests public_repos with license
+        """
+        self.assertEqual(
+            GithubOrgClient('google').public_repos(license='apache-2.0'),
+            self.apache2_repos,
+        )
 
     @classmethod
     def tearDownClass(cls) -> None:
